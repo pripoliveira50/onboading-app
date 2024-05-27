a5941ea48df00f0874af4d000b48ed82f2f00057
Priscila Oliveira <poliveira13@hotmail.com>
2024-05-26 21:53:59 -0300

    feat: create absolute path babel

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@assets": "./assets",
+          },
+        },
+      ],
+    ],
+  };
+};
